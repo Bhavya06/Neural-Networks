49cbb37bbc34365d963a284b66973551f6ce8d09
Dev Bhartra <devbhartra183@gmail.com>
2019-11-15 16:39:19 +0530

    Updated roseAnn.py

--- roseAnn.py
+++ roseAnn.py
@@ -4,74 +4,64 @@ import numpy as np
 from datetime import datetime
 import pandas as pd
 from sklearn.metrics import accuracy_score
+from datetime import datetime
+import numpy as np
+
+startTime = datetime.now()
+print("\n\n\nStarting the execution now:\n")
 
 l = []
 def generateColumns(start, end):
     for i in range(start, end+1):
         l.extend([str(i)+'X', str(i)+'Y'])
     return l
-
 eyes = generateColumns(1, 12)
-
 import pandas as pd
 df = pd.read_csv('Eyes.csv')
+first_column = df.columns[0]
+df = df.drop([first_column],axis = 1)
 # print("hello")
 X = df[eyes]
 y = df['truth_value']   #the actual class labels
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10)
-
 # Data Normalization
 from sklearn.preprocessing import StandardScaler as SC
 sc = SC()
 X_train_scaled = sc.fit_transform(X_train)
 X_test_scaled = sc.fit_transform(X_test)
 #not scaling y since it's already 0s and 1s
-
-import numpy as np
 X_train, y_train, X_test, y_test = np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)
 #converting all the scaled data to numpy arrays
-'''
-nn_model1 = mlrose.NeuralNetwork(hidden_nodes = [2], activation ='relu', 
-                                 algorithm ='random_hill_climb', 
-                                 max_iters = 1000, bias = True, is_classifier = True, 
-                                 learning_rate = 0.0001, early_stopping = True, 
-                                 clip_max = 5, max_attempts = 100, random_state = 3)
 
-nn_model1.fit(X_train_scaled, y_train)
+acc = 0
 
-# print('Works so far')
+def gradDesc():
+    nn_model2 = mlrose.NeuralNetwork(hidden_nodes = [2], activation = 'relu', 
+                                    algorithm = 'gradient_descent', 
+                                    max_iters = 5000, bias = True, is_classifier = True, 
+                                    learning_rate = 0.0001, early_stopping = True, 
+                                    clip_max = 5, max_attempts = 100, random_state = 3)
 
-y_train_pred = nn_model1.predict(X_train_scaled)
+    nn_model2.fit(X_train_scaled, y_train)
 
-y_train_accuracy = accuracy_score(y_train, y_train_pred)
+    # y_train_pred = nn_model2.predict(X_train_scaled)
+    # y_train_accuracy = accuracy_score(y_train, y_train_pred)
+    # print("The Training accuracy is: ",y_train_accuracy*100,"%")
 
-print(y_train_accuracy)
+    y_test_pred = nn_model2.predict(X_test_scaled)
+    y_test_accuracy = accuracy_score(y_test, y_test_pred)
+    return (y_test_pred, y_test_accuracy)
+# acc = y_test_accuracy*100
 
-y_test_pred = nn_model1.predict(X_test_scaled)
+y_test_accuracy = gradDesc()
+# print(y_test_accuracy)
+if (y_test_accuracy[1] > (acc*100) ):
+    print("Higher accuracy found")
 
-y_test_accuracy = accuracy_score(y_test, y_test_pred)
 
-print(y_test_accuracy)
-'''
 
-nn_model2 = mlrose.NeuralNetwork(hidden_nodes = [2], activation = 'relu', 
-                                 algorithm = 'gradient_descent', 
-                                 max_iters = 1000, bias = True, is_classifier = True, 
-                                 learning_rate = 0.0001, early_stopping = True, 
-                                 clip_max = 5, max_attempts = 100, random_state = 3)
+print("The Testing accuracy is: ",y_test_accuracy[1]*100,"%")
 
-nn_model2.fit(X_train_scaled, y_train)
-
-y_train_pred = nn_model2.predict(X_train_scaled)
-
-y_train_accuracy = accuracy_score(y_train, y_train_pred)
-
-print(y_train_accuracy)
-
-y_test_pred = nn_model2.predict(X_test_scaled)
-
-y_test_accuracy = accuracy_score(y_test, y_test_pred)
-
-print(y_test_accuracy)
+print("\n")
+print("Execution time in seconds = ", datetime.now() - startTime)
